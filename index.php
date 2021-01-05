167dd716a6c88db80681916b226c853f6fbcdc3f
VovaGul <vovagul1999@mail.ru>
2021-01-05 15:15:33 +0500

    Добавьте файлы проекта.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+function RecommendVideos(string $user_id){
+
+    $videosThisSemester = GetALLVideosThisSemester($user_id);
+
+}
+
+// объ€вление метода
+function GetALLVideosThisSemester(string $user_id) {
+
+    return $currentTime;
+}
+
+
+echo GetALLVideosThisSemester("sdfds")
+?>
